40f48c100c2794b0b1ad766af03da8f31bce164e
onigirikk <122284491+onigirikk@users.noreply.github.com>
2023-04-18 16:34:28 +0000

    Update_orderpage&mypage

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,18 +4,35 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = Order.all
-    # @address = Address.find(params[:order][:address_id])
-    # @order.shipping_postal_code = @address.postal_code
-    # @order.shipping_address = @address.address
-    # @order.shipping_name = @address.name
+    @order = Order.new(order_params)
     @cart_items = current_customer.cart_items.all
+    @shipping_cost = 800
+    if params[:order][:address_select] == "0"
+     @order.shipping_postal_code = current_customer.postal_code
+     @order.shipping_address = current_customer.address
+     @order.shipping_name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_select] == "1"
+      @address = Address.find(params[:order][:address_id])
+    else
+      @order.shipping_postal_code = @order.shipping_postal_code
+      @order.shipping_address = @order.shipping_address
+      @order.shipping_name = @order.shipping_name
+    end
   end
 
   def complete
+    current_customer.cart_items.destroy_all
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+    # 商品があるかどうかif文で分岐させる
+    if CartItem.find_by()
+      # ある場合は個数をフォームから送られた個数分追加する
+    else
+    # 　カートモデルにレコードを新規作成する
+    end
   end
 
   def index
@@ -26,6 +43,6 @@ class Public::OrdersController < ApplicationController
   
   private
   def order_params
-    params.require(:order).permit(:payment_method, :address_select, :customer_id, :shipping_postal_code, :shipping_address, :shipping_name, :shipping_cost, :total_payment, :status)
+    params.require(:order).permit(:payment, :customer_id, :shipping_postal_code, :shipping_address, :shipping_name, :shipping_cost, :total_payment, :status)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
             image.variant(resize_to_limit:[width, height]).processed
     end
     
+    # 消費税を求めるメソッド
     def with_tax_price
         (price*1.1).floor
     end
