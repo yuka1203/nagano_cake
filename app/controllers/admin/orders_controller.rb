45d48950e3436bd438a88b69e1ae09232f914154
onigirikk <122284491+onigirikk@users.noreply.github.com>
2023-04-28 08:04:08 +0000

    update layout

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,9 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @order_details = OrderDetail.all
+    @order = Order.find(params[:id])
   end
+  
+  private
+  def order_params
+    params.require(:order).permit(:status)
 end
